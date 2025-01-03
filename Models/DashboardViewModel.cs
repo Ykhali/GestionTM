d6254729667367b457549088ea290370fc3ef567
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-01-03 15:29:45 +0100

    second

---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GestionTM.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int Created { get; set; }
+        public int Submitted { get; set; }
+        public int Accepted { get; set; }
+        public int Refused { get; set; }
+    }
+}
+
